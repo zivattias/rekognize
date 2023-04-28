bd2630fd662b5ac9dcac1ce56e610b2fdb5c0f70
Ziv Attias <ziv.attias7@gmail.com>
2023-04-28 16:03:19 +0300

    modify: API consts of deployed AWS EC2 server

--- rekognize/src/consts.ts
+++ rekognize/src/consts.ts
@@ -1,5 +1,5 @@
 const API = {
-    URL: process.env.NODE_ENV == "development" ? "http://0.0.0.0:" : "https://rekognize.vercel.app",
+    URL: process.env.NODE_ENV == "development" ? "http://0.0.0.0:" : "ec2-54-85-78-242.compute-1.amazonaws.com/api",
     PORT: process.env.NODE_ENV == "development" ? "8000" : "",
 }
 
