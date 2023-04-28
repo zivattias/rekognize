0ecc4e49cc6d2707e4a1476d9eb7c8f6f9c8dd6f
Ziv Attias <ziv.attias7@gmail.com>
2023-04-28 16:06:55 +0300

    modify: API consts of deployed AWS EC2 server

--- rekognize/src/consts.ts
+++ rekognize/src/consts.ts
@@ -1,13 +1,13 @@
 const API = {
-    URL: process.env.NODE_ENV == "development" ? "http://0.0.0.0:" : "ec2-54-85-78-242.compute-1.amazonaws.com/api",
-    PORT: process.env.NODE_ENV == "development" ? "8000" : "",
+    URL: process.env.NODE_ENV == "development" ? "http://0.0.0.0:" : "http://ec2-54-85-78-242.compute-1.amazonaws.com/api",
+    PORT: "8000",
 }
 
 const ENDPOINTS = {
-    ROOT: "/",
+    ROOT: "/api",
     FILESIZE: "/filesize/",
     UPLOAD: "/upload/"
 }
 
-export const FULL_API_URL = API.URL + API.PORT
+export const FULL_API_URL = process.env.NODE_ENV == "development" ? API.URL + API.PORT + ENDPOINTS.ROOT : API.URL
 export const API_UPLOAD_URL = FULL_API_URL + ENDPOINTS.UPLOAD
