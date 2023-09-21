dd9014fac1d66534d2388ec8566d7f6066647471
Ziv Attias <ziv.attias7@gmail.com>
2023-09-21 16:51:55 +0300

    Update API URL

--- rekognize/src/consts.ts
+++ rekognize/src/consts.ts
@@ -1,13 +1,19 @@
 const API = {
-    URL: process.env.NODE_ENV == "development" ? "http://0.0.0.0:" : "http://ec2-54-85-78-242.compute-1.amazonaws.com/api",
-    PORT: "8000",
-}
+  URL:
+    process.env.NODE_ENV == "development"
+      ? "http://0.0.0.0:"
+      : "http://ec2-3-85-12-62.compute-1.amazonaws.com/api",
+  PORT: "8000",
+};
 
 const ENDPOINTS = {
-    ROOT: "/api",
-    FILESIZE: "/filesize/",
-    UPLOAD: "/upload/"
-}
+  ROOT: "/api",
+  FILESIZE: "/filesize/",
+  UPLOAD: "/upload/",
+};
 
-export const FULL_API_URL = process.env.NODE_ENV == "development" ? API.URL + API.PORT + ENDPOINTS.ROOT : API.URL
-export const API_UPLOAD_URL = FULL_API_URL + ENDPOINTS.UPLOAD
+export const FULL_API_URL =
+  process.env.NODE_ENV == "development"
+    ? API.URL + API.PORT + ENDPOINTS.ROOT
+    : API.URL;
+export const API_UPLOAD_URL = FULL_API_URL + ENDPOINTS.UPLOAD;
